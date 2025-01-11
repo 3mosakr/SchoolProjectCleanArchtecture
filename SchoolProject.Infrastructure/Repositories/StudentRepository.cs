a8cee15e1677be370f6fb91e9aa6ab3a309abd91
Mahmoud Sakr <sakr364@outlook.com>
2025-01-11 23:12:54 +0200

    Responce & Responce handler, Generic Repository

--- SchoolProject.Infrastructure/Repositories/StudentRepository.cs
+++ SchoolProject.Infrastructure/Repositories/StudentRepository.cs
@@ -2,6 +2,7 @@
 using SchoolProject.Data.Entities;
 using SchoolProject.Infrastructure.Abstracts;
 using SchoolProject.Infrastructure.Data;
+using SchoolProject.Infrastructure.InfrastructureBases;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,25 +11,23 @@ using System.Threading.Tasks;
 
 namespace SchoolProject.Infrastructure.Repositories
 {
-    public class StudentRepository : IStudentRepository
+    public class StudentRepository : GenericRepositoryAsync<Student>, IStudentRepository
     {
         #region Fields
-        private readonly ApplicationDbContext _dbContext;
+        private readonly DbSet<Student> _students;
         #endregion
 
         #region Constructor
-        public StudentRepository(ApplicationDbContext dbContext)
+        public StudentRepository(ApplicationDbContext dbContext): base(dbContext)
         {
-            _dbContext = dbContext;
+            _students = dbContext.Set<Student>();
         }
         #endregion
 
         #region Handle Methods
         public async Task<List<Student>> GetStudentsListAsync()
         {
-            return await _dbContext.Students
-                .Include(s => s.Department)
-                .ToListAsync();
+            return await _students.Include(s => s.Department).ToListAsync();
         }
         #endregion
     }
